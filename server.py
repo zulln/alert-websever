0809fe6b7aaa02bf15c41a52c4d7f00dfcccdd13
Linus Särud <linus@sarud.se>
2025-03-19 20:29:49 +0100

    changed to port 8080

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def catch_all(path):
     return Response('alert(origin)', mimetype='application/javascript')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
